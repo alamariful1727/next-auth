8c114f4b3a2bbc1002a357550ccd6d66f8a682e9
Ariful Alam <aalam@pelmorex.com>
2024-12-17 14:51:04 -0500

    feat: Alias path added

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import Header from "../components/header";
+import Header from "@components/header";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
