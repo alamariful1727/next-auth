0d4ab206c8da4e18ac64391cb7599c04d41ac4ea
Ariful Alam <aalam@pelmorex.com>
2024-12-20 13:11:20 -0500

    feat: AuthProvider added

--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,5 +1,28 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@components/index";
+
 const HomePage = () => {
-  return <div className="container mx-auto">HomePage</div>;
+  const { isAuthenticated, user, logout } = useAuth0();
+
+  const handleLogout = async () => {
+    await logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+  return (
+    <div className="container mx-auto">
+      {isAuthenticated ? (
+        <div className="mt-10 flex flex-col items-center space-y-2">
+          <p>Welcome to the authenticated home page!</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <Button theme="primary" onClick={handleLogout}>
+            Logout
+          </Button>
+        </div>
+      ) : (
+        <p>Welcome to the home page!</p>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
